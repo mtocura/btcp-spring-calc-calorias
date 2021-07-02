c5dbd333a3275e5755793d5a527f6bf02d428b20
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 11:16:26 -0300

    Inicialização do projeto spring boot

---
+++ src/main/java/br/com/aula3/tm/calculadoracaloria/CalculadoraCaloriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.aula3.tm.calculadoracaloria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraCaloriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraCaloriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/aula3/tm/calculadoracaloria/CalculadoraCaloriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula3.tm.calculadoracaloria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraCaloriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
